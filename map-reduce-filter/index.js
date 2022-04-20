365ee88bf70eaabb5c27f04409e109a8c0455c28
mitcareem <mitcareem@gmail.com>
2022-04-20 16:15:31 +0530

    initial commit

---
+++ map-reduce-filter/index.js
@@ -0,0 +1,74 @@
+// map fucntion is used to transformation of an array.......
+
+// const arr = [1, 2, 3, 4];
+
+// const double = (x) => {
+//     return x * 2;
+// }
+
+// const output = arr.map(double);
+
+// console.log(output);
+
+
+// const arr = [1, 2, 3, 4];
+
+// const binary = (x) => {
+//     return x.toString(2);
+// }
+
+// const output = arr.map(binary);
+
+// console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// filter function is used to filter the value inside the array
+// which are filtered based on the some logic
+
+// const arr = [1, 2, 3, 4];
+
+// // try to filter out all the even values inside above array
+
+// const isEven = (x) => {
+//     return x % 2 === 0;
+// }
+
+// const output = arr.filter(isEven);
+// console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+// reduce is used to find sum or max or min of array
+
+const arr = [1, 2, 3, 4];
+
+const output = arr.reduce(function(acc, curr) {
+    acc = acc + curr;
+    return acc;
+}, 0);
+
+console.log(output);
+
+
+
+
