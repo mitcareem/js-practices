01b10d22448283fbac371ac16c23a064c53c258d
mitcareem <mitcareem@gmail.com>
2022-07-16 08:32:55 +0530

    react addes

--- Weather-API/index.js
+++ Weather-API/index.js
@@ -1,5 +1,5 @@
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Mohammad";
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Mohammad";
 
-fetch(url)
-.then(response => response.json())
-.then(json => console.log(json))
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
--- map-reduce-filter/index.js
+++ map-reduce-filter/index.js
@@ -2,24 +2,24 @@
 
 // const arr = [1, 2, 3, 4];
 
-// const double = (x) => {
-//     return x * 2;
-// }
+const double = (x) => {
+    return x * 2;
+}
 
-// const output = arr.map(double);
+const output = arr.map(double);
 
-// console.log(output);
+console.log(output);
 
 
-// const arr = [1, 2, 3, 4];
+const arr = [1, 2, 3, 4];
 
-// const binary = (x) => {
-//     return x.toString(2);
-// }
+const binary = (x) => {
+    return x.toString(2);
+}
 
-// const output = arr.map(binary);
+const output = arr.map(binary);
 
-// console.log(output);
+console.log(output);
 
 
 
@@ -37,7 +37,7 @@
 // filter function is used to filter the value inside the array
 // which are filtered based on the some logic
 
-// const arr = [1, 2, 3, 4];
+// /const arr = [1, 2, 3, 4];
 
 // // try to filter out all the even values inside above array
 
@@ -46,6 +46,7 @@
 // }
 
 // const output = arr.filter(isEven);
+// console.log(arr);
 // console.log(output);
 
 
@@ -60,14 +61,14 @@
 
 // reduce is used to find sum or max or min of array
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-const output = arr.reduce(function(acc, curr) {
-    acc = acc + curr;
-    return acc;
-}, 0);
+// const output = arr.reduce(function(acc, curr) {
+//     acc = acc + curr;
+//     return acc;
+// }, 0);
 
-console.log(output);
+// console.log(output);
 
 
 
